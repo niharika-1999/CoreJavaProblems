332fa3308f10051de033f8e89785c62299cbf21e
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-22 13:20:10 +0530

    Added:VowelNConsonant

---
+++ src/com/bridgelabz2/VowelConsonant.java
@@ -0,0 +1,14 @@
+package com.bridgelabz2;
+
+public class VowelConsonant {
+	public static void main(String[] args) {
+		char ch = 'a';
+		
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			System.out.println(ch + "is a Vowel");
+		else
+			System.out.println(ch + "is a Consonant");
+	}
+
+
+}
