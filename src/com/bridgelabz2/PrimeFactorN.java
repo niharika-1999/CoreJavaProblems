c72046b89530515c62dfaa20873cfcf396c492e7
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-22 12:22:38 +0530

    Added : PrimeFactorN

---
+++ src/com/bridgelabz2/PrimeFactorN.java
@@ -0,0 +1,21 @@
+package com.bridgelabz2;
+import java.util.Scanner;
+
+public class PrimeFactorN {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println("Prime Factor are:");
+		    for(int i=2;i<=num;i++)
+		    {
+		    	while(num%i==0) {
+		    		System.out.print(i+"");
+		    		num=num/i;
+		    	}
+		    }
+		    if(num<1)
+		    	System.out.println(num);
+	}
+
+}
